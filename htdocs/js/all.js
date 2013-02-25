9f0796b003b0171b54f1a234ca5f5ec3267a9968
kazukgw <kazugkgw@gmail.com>
2013-02-25 23:15:40 +0900

    素材変更,光源追加,オブジェクトデータ変更

--- htdocs/js/all.js
+++ htdocs/js/all.js
@@ -35837,16 +35837,22 @@ function init() {
 	 * @type {THREE}
 	 */
 	camera = new THREE.PerspectiveCamera(
-		50,  // fov — camera frustum vertical field of view.
+		20, // fov — camera frustum vertical field of view.
 		window.innerWidth / window.innerHeight,  // aspect — camera frustum aspect ratio.
 		10, // near — camera frustum near plane.
-		1000 // far — camera frustum far plane.
+		30 // far — camera frustum far plane.
 	);
 	// 高さ初期位置
 	camera.position.z = 20;
 	// camera.position.y = 0;
 
 	scene = new THREE.Scene();
+
+	//光源を追加
+	var light = new THREE.DirectionalLight( 0xf4a4d4 );
+	light.position.set( -1, -1, 2 ).normalize();
+	scene.add( light );
+
 /****** Create Object *******/
 	// オブジェクトの構造を定義
 	// var geometry = new THREE.CubeGeometry( 200, 200, 200 );
@@ -35858,23 +35864,24 @@ function init() {
 		callback — required. This function will be called with the loaded model as an instance of geometry when the load is completed.
 		texturePath — optional. If not specified, textures will be assumed to be in the same folder as the Javascript model file.
 	*/
-	loader.load( "../sample_data/untitled.js", createScene );
+	// オブジェクトデータがロードし終わったら、 creatSceneを実行する
+	loader.load( "../sample_data/monky3.js", createScene );
 
 	window.addEventListener( 'resize', onWindowResize, false );
 }
-
+//第一引数の gemmetry は loader.load()によってローダされたオブジェクトのデータがわたされる
 function createScene( geometry ) {
-	var texture = THREE.ImageUtils.loadTexture( 'textures/crate.gif' );
-	texture.anisotropy = renderer.getMaxAnisotropy();
-
-	geometry.morphTargets = [];
-	var material = new THREE.MeshPhongMaterial({
-		color: 0xffffff,
-		specular: 0xcccccc,
-		shininess:50,
-		ambient: 0xffffff,
+	// var texture = THREE.ImageUtils.loadTexture( 'textures/crate.gif' );
+	// texture.anisotropy = renderer.getMaxAnisotropy();
+	// geometry.morphTargets = [];
+	var material = new THREE.MeshLambertMaterial({
+		color: 0xf5a5b5,
+		specular: 0xf5a5b5,
+		shininess:100,
+		ambient: 0xf5a5b5,
 		side: THREE.DoubleSide
 	});
+
 
 	mesh = new THREE.Mesh( geometry, material );
 	// シーンの作成
